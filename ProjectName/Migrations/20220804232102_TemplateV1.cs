e9c2a3b4748fddc47816d739140dab8334201f0f
Kyle Chinick <kylechinick@icloud.com>
2022-08-04 16:26:27 -0700

    Remove leftover references to Specialty field
    Co-authored-by: Claire Thorington <claire.thorington@gmail.com>
    Co-authored-by: Caroline Cerussi <caroceru@gmail.com>

---
+++ ProjectName/Migrations/20220804232102_TemplateV1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectName.Migrations
+{
+    public partial class TemplateV1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Specialty",
+                table: "Categories");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Specialty",
+                table: "Categories",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+    }
+}
--- ProjectName/Models/Category.cs
+++ ProjectName/Models/Category.cs
@@ -8,15 +8,11 @@ namespace ProjectName.Models
     {
       this.JoinEntities = new HashSet<CategoryItem>();
 
-      // this.JoinEntities2 = new HashSet<CategorySpecialty>();
-
     }
 
     public int CategoryId { get; set; }
 
     public string Name { get; set; }
-
-    public string Specialty { get; set; }
 
     public virtual ICollection<CategoryItem> JoinEntities { get; set; }
   }
